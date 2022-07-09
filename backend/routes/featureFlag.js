94d3157437a876262a09b1c7ecb298960a80a41d
Neha Sharma <nsharma215@gmail.com>
2022-07-09 09:03:47 +0100

    Edit flag (#37)
    * 🕵️‍♂️ find a flag
    
    * test-cases added
    
    * 😎 code-refactor + test-cases added
    
    * edit featureflag + code-refactor
    
    * ✍️ edit feature flag and more code refactored 🤪

--- backend/routes/featureFlag.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const createFeatureFlag = require('../controllers/createFeatureFlag');
-const router = express.Router();
-
-/* Create Feature Flag */
-router.post('/', createFeatureFlag);
-
-module.exports = router;
