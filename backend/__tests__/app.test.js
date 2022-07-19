f12ef70133174b465b89ae6ef1125e397506f9e3
neha <nsharma215@gmail.com>
2022-07-19 14:54:34 +0100

    fixed wrong file path

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -1,16 +1,6 @@
 const supertest = require('supertest');
 const app = require('../app');
 
-const flagMockPayload = {
-    name:'test-ff-mock',
-    role: ['mock-1','mock-2'],
-    isEnable: false,
-    createdOn:'12-03-2002',
-    createdBy:'mock-user',
-    modifyBy: 'mock-admin',
-    modifyOn:'12-03-2002'
-}
-
 describe("create and get feature flags", () => {
     it("should get 200 for GET request to /featureflags", async () => {
         await supertest(app).get(`/featureflags`).expect(200)
