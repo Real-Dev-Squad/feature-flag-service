6fce656ed3ccf36057ca71f185893798d755bdfb
neha <nsharma215@gmail.com>
2022-07-20 21:42:45 +0100

    removed the commented test cases

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -8,11 +8,4 @@ describe("create and get feature flags", () => {
     it("should throw 404 for accessing wrong URL", async () => {
         await supertest(app).get(`/featureflas`).expect(404)
     })
-    // it("should return 404 if flag is not there ", async () => {
-    //     const flagName = 123;
-    //     await supertest(app).get(`/featureflags/${flagName}`).expect(404)
-    // })
-    // it("should return 404 for incorrect POST request", async () => {
-    //     await supertest(app).post("/featureflags").expect(404);
-    // })
 })
