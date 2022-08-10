c82bc36a2b97eb6bd74bc3bf9138cabbcea4ff0c
neha <nsharma215@gmail.com>
2022-08-10 08:44:51 +0100

    Updated eslint

--- ui/src/Components/Layout/Layout.js
+++ ui/src/Components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import LeftNav from '../LeftNav/LeftNav';
+import { USER } from '../../test/Fixtures/user';
 
 export default function Layout(props){
     return(
         <>
-        <Header />
+        <Header user={USER}/>
             <main className='flex flex-row my-4'>
                 <LeftNav />
                 <section> 
