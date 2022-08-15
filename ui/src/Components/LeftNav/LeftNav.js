29bc820bdff4088729963b822264ee720e5adaf3
neha <nsharma215@gmail.com>
2022-08-15 08:18:30 +0100

    Left Navigation test cases added

--- ui/src/Components/LeftNav/LeftNav.js
+++ ui/src/Components/LeftNav/LeftNav.js
@@ -4,13 +4,13 @@ import { NAV } from '../../Constant/constant';
 export default function LeftNav(){
     const createNavigation = () => {
         return Object.keys(NAV).map(function(key){
-            return <li key={key} className='text-blue-600 hover:underline text-sm py-1'>
+            return <li role="listitem" key={key} className='text-blue-600 hover:underline text-sm py-1'>
             <Link to={NAV[key]}>{key}</Link> </li>
         })
     }
     return(
         <nav className='px-3'>
-            <ul>
+            <ul role="list">
                {createNavigation()} 
             </ul>
         </nav>
--- ui/src/test/LeftNav.test.js
+++ ui/src/test/LeftNav.test.js
@@ -1,9 +1,52 @@
-import { render } from '@testing-library/react';
-import { Link,MemoryRouter as Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { Link, MemoryRouter as Router } from 'react-router-dom';
 import LeftNav from '../Components/LeftNav/LeftNav';
+
+const NAV = {
+    "Home" : "/",
+    "Create Users" : "/users",
+    "Flags" : "/flags",
+    "SDK" : "/sdk",
+    "Docs" : "/docs",
+}
 
 describe('<LeftNav/> component', () => {
     it('component should render',() => {
         render(<Router><LeftNav /></Router>);
+    });
+    it('should have UL',() => {
+        render(<Router><LeftNav /></Router>);
+        const element = screen.getByRole('list');
+        expect(element).toBeInTheDocument();
+    });
+    it('should have LIs',() => {
+        render(<Router><LeftNav /></Router>);
+        const element = screen.getAllByRole('listitem');
+        expect(element).toHaveLength(5);
+    });
+    it('should have Home',() => {
+        render(<Router><LeftNav /></Router>);
+        const element = screen.getByRole('link',{ name: 'Home', href: '/'});
+        expect(element).toBeInTheDocument();
+    });
+    it('should have Create Users',() => {
+        render(<Router><LeftNav /></Router>);
+        const element = screen.getByRole('link',{ name: 'Create Users', href: '/users'});
+        expect(element).toBeInTheDocument();
+    });
+    it('should have Flags',() => {
+        render(<Router><LeftNav /></Router>);
+        const element = screen.getByRole('link',{ name: 'Flags', href: '/flags'});
+        expect(element).toBeInTheDocument();
+    });
+    it('should have SDK',() => {
+        render(<Router><LeftNav /></Router>);
+        const element = screen.getByRole('link',{ name: 'SDK', href: '/sdk'});
+        expect(element).toBeInTheDocument();
+    });
+    it('should have Docs',() => {
+        render(<Router><LeftNav /></Router>);
+        const element = screen.getByRole('link',{ name: 'Docs', href: '/docs'});
+        expect(element).toBeInTheDocument();
     });
 })
