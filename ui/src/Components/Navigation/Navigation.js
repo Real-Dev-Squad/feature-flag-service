eb703317869ff00711b5a33156cef327e468f936
neha <nsharma215@gmail.com>
2022-09-24 22:39:11 +0100

    fixed build issues

--- ui/src/Components/Navigation/Navigation.js
+++ ui/src/Components/Navigation/Navigation.js
@@ -4,13 +4,13 @@ import { NAV } from '../../Constant/routes';
 export default function LeftNav(){
     const createNavigation = () => {
         return NAV.map((item, key) => {
-            return <li role="listitem" key={key} className='text-blue-600 hover:underline text-sm py-1'>
+            return <li key={key} className='text-blue-600 hover:underline text-sm py-1'>
              <NavLink to={item.path}>{item.name}</NavLink> </li>
         })
     }
     return(
         <nav className='px-3'>
-            <ul role="list">
+            <ul>
                {createNavigation()} 
             </ul>
         </nav>
